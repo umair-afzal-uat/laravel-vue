6b9f50a4b8b20b603c4a536fe57f89f593f1d30b
Umair Afzal <128166400+umair-afzal-uat@users.noreply.github.com>
2024-10-23 04:41:43 -0700

    chore: initial commit

---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Hello, Vue with TypeScript!</h1>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
